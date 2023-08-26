9c23b7396658acecbce03be6bb88840a68e586cd
Gaurav soni <gauravsoni@Gauravs-MacBook.local>
2023-08-26 14:18:16 +0400

    Added css modules

---
+++ 06-html-css-project-css-modules/index.js
@@ -0,0 +1,17 @@
+import "./css/styles.css";
+import 'bootstrap';
+
+const signinBtn = document.getElementById("signin-btn");
+const usernameTxt = document.getElementById("username-txt");
+const passwordTxt = document.getElementById("password-txt");
+
+signinBtn.addEventListener("click", function () {
+    const username = usernameTxt.value;
+    const password = passwordTxt.value;
+
+    if (username === "admin" && password === "admin") {
+        window.location.href = "/pages/dashboard.html";
+    } else {
+        alert("Wrong username or password");
+    }
+});
